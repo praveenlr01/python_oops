3c8d4bd14a3b21ec1183932f359eb811ae7af237
Praveenkumar <100259959+praveenlr01@users.noreply.github.com>
2022-03-22 16:59:53 +0530

    a2b_restaurant_FAQ_chatbox

---
+++ a2b_chatbox.py
@@ -0,0 +1,53 @@
+print("\tWelcome you to \"Adayar Ananda Bhavan\"(veg.Restaurant)")
+print("How can I help you?")
+answers=[" hai"," hello"," 7AM - 10.30PM"," Yes.we have car parking in front of restaurant with no charges.","\tMORNING MENU\t","\nIDLI--45 \nSAMABAR IDLI--55 \nRAVA IDLI--50 \nIDIYAPPAM--60 \nDOSAI \nPLAIN DOSAI--65\nONION DOSAI--75\nPODI DOSAI--85\nMASALA DOSAI--90\nA2B COMBOS MINI TIFFIN--90","\tLUNCH MENU\t","\nVaraity rice(sambar-lemon-tomoto-curd)--55\nfull meals--110","\tDINNER\t","\nChapathi--65\nPorota--75 \nBEVERAGES\nMASALA TEA\nROSE MILK --65\nMANGO LASSI--75\nSTRAWBERRY MILKSHAKE--85\nAVACADO MILKSHAKE--85"," NO,it is pure veg restaurant."," thank you.we also love you!"," sorry.I can't understand.."," you are my creator"," yes.we serve north indian dishes in dinner time","Cash\ndebit\\credit Cards\nUPI transations."," Sure.use the link below","https://a2bva.com/order-online/"," Thank you for visiting"]
+
+def hai():
+    print(answers[0])
+def hello():
+    print(answers[1])
+def timing():
+    print(answers[2])
+def menu():
+    me=int(input("[1]tiffin[2]lunch[3]dinner[4]All menu\nEnter: "))
+    if me==1:
+        print(answers[4],answers[5])
+    elif me==2:
+        print(answers[6],answers[7])
+    elif me==3:
+        print(answers[8],answers[9])
+    else:
+        print(answers[4],answers[5])
+        print(answers[6],answers[7])
+        print(answers[8],answers[9])
+def parking():
+    print(answers[3])
+def nonveg():
+    print(answers[10])
+def love():
+    print(answers[11])
+def sorry():
+    print(answers[12])
+def creator():
+    print(answers[13])
+def north():
+    print(answers[14])
+def payment():
+    print(answers[15])
+def online():
+    print(answers[16])
+    print(answers[17])
+def table():
+    print(answers[16])
+    print(answers[17])
+def thank():
+    print(answers[18])
+
+ques={"hai":hai,"hello":hello,"timing":timing,"menu":menu,"car parking":parking,"nonveg is available":nonveg,"i love you":love,"who is your creator":creator,"do you serve north indian dishes":north,"payment option":payment,"can i order through online":online,"can i book table":table,"thank you":thank}
+
+while True:
+    ab=input()
+    if ab in ques:
+        ques[ab]()
+    else:
+        sorry()
